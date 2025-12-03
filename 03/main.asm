4d9e63813d7747a364e31b95a4b3e7b061f2ecf3
tymon <tymon.mieszkowski@gmail.com>
2025-12-03 19:29:02 +1300

    day 3.2

--- 03/main.asm
+++ 03/main.asm
@@ -8,6 +8,7 @@ global _start
 
 _start:
     call _first
+    call _second
 
     xor edi, edi
     mov rax, 0x2000001 ; macOS exit syscall (0x2000000 + 1)
@@ -58,9 +59,53 @@ _first:
     call _write_int
     ret
 
+_second:
+    mov qword [output], 0
+    mov rcx, input_lines
+    lea rdi, [input] ; cursor
+
+.process_line:
+    mov r8, 0 ; current line answer
+    mov rdx, battery_size ; how far to look ahead
+    mov r9, rdi ; memory location of max for current char
+    jmp .process_loop
+.reset_cursor:
+    mov rdi, r9
+    inc rdi ; start looking from next char
+    inc r9
+.process_loop:
+    mov al, byte [rdi]
+    cmp al, byte [r9]
+    jle .next_char
+    mov r9, rdi
+.next_char:
+    cmp byte [rdi + rdx], 10 ; newline
+    je .hit_end_line
+
+    inc rdi
+    jmp .process_loop
+
+.hit_end_line:
+    mul r8, 10
+    movzx rbx, byte [r9]
+    add r8, rbx
+    sub r8, '0'
+    dec rdx ; use one more char this time
+    jnz .reset_cursor
+
+    ; line is fully processed now
+    add [output], r8
+    add rdi, 2 ; skip last char and newline
+    dec rcx ; line counter
+    jnz .process_line
+.done:
+    mov rdi, [output]
+    call _write_int
+    ret
 
 section .data
 %include "03/input.inc"
+battery_size: equ 12
 
 section .bss
 output: resb 8
